da39f16699e38ccbcb65d36e735a3575f751d211
brianramaswami <brianramaswami@yahoo.com>
2019-08-22 10:31:24 -0700

    DONE

--- js/main.js
+++ js/main.js
@@ -31,3 +31,26 @@ function toggleMenu() {
     showMenu = false;
   }
 }
+
+// creating a pop-up for user
+let rps = document.getElementById("rpsDiv");
+rps.onclick = function(e){
+    e.preventDefault();
+    window.open ("https://brianramaswami.github.io/RPS-Multiplayer/","game1","menubar=1,resizable=1,width=400,height=825");
+    console.log("window 1.");
+    setTimeout(() => {
+    window.open ("https://brianramaswami.github.io/RPS-Multiplayer/","game2","menubar=1,resizable=1,width=400,height=825,left=500"), 6000
+    });
+    console.log("window 2.")
+}
+
+let rps1 = document.getElementById("rpsDiv1");
+rps1.onclick = function(e){
+    e.preventDefault();
+    window.open ("https://brianramaswami.github.io/RPS-Multiplayer/","game1","menubar=1,resizable=1,width=400,height=825");
+    console.log("window 1.");
+    setTimeout(() => {
+    window.open ("https://brianramaswami.github.io/RPS-Multiplayer/","game2","menubar=1,resizable=1,width=400,height=825,left=500"), 6000
+    });
+    console.log("window 2.")
+}
